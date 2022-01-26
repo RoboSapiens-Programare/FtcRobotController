0e9e693892e7c61bb16f5f6d546df787bac6685e
Victor <victor.hasieganu@gmail.com>
2022-01-26 21:10:51 +0200

    organizat cod plus adaugat autonomie
    am organizat codul ca sa fie mai usor de navigat si am adaugat clasa de autonomie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Drive/Opmode/Autonomous.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.PoseStorage;
+import org.firstinspires.ftc.teamcode.drive.Subsystem;
+import org.firstinspires.ftc.teamcode.drive.localization.vision.RingStackDeterminationPipeline;
+import org.firstinspires.ftc.teamcode.drive.tank.Robot;
+
+@Autonomous(name="Autonomie Standard", group = "autonomous")
+public class AutonomousMain extends LinearOpMode {
+
+    private Robot robot;
+    public static int MAX_MILISECONDS = 2500;
+    private static final double FOAM_TILE_INCH = 23.6;
+    private double startAngle, wobbleSpeed;
+
+    private final Pose2d startPose = new Pose2d(-2.6 * FOAM_TILE_INCH, -1 * FOAM_TILE_INCH, Math.toRadians(-90));
+    private final Vector2d parkingVector = new Vector2d(0.5 * FOAM_TILE_INCH,-2.3 * FOAM_TILE_INCH);
+    //Modificat aceste obiecte
+    private final Vector2d secondWobble = new Vector2d(-1.3 * FOAM_TILE_INCH, -2.4 * FOAM_TILE_INCH);
+    private Pose2d wobbleDropPose = new Pose2d(0.2 * FOAM_TILE_INCH, -1 * FOAM_TILE_INCH);
+    private static double endTargetTangent = Math.toRadians(-180);
+    private static double startTargetTangent = Math.toRadians(60);
+
+    private final Pose2d throwWobble = new Pose2d(-0.2*FOAM_TILE_INCH, -1.3 * FOAM_TILE_INCH, Math.toRadians(0));
+
+    private RingStackDeterminationPipeline.RingPosition numberOfRing = RingStackDeterminationPipeline.RingPosition.NONE;
+
+    public void initAutonomous(){
+        telemetry.addData(">", "Initializing...");
+        telemetry.update();
+
+        robot = new Robot(hardwareMap);
+        
+        // initiazilare;
+
+        robot.prototipSweeper.servoBrat.setPosition(0.3);
+
+        robot.prototipSweeper.servoBrat2.setPosition(0.7);
+
+        robot.openCV.start();
+
+            while (robot.isInitialize() && opModeIsActive()) {
+                idle();
+            }
+
+            telemetry.addData(">", "Initialized");
+            telemetry.update();
+        }
+        /*  task 1: plasat obiect pe shipping hub
+            task 2: mutat ratuste de pe carusel
+            task 3: parcat in patrat/warehouse
+        */
+}
